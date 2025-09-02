39628763acaec0946aaded2e4f1f17a54150408e
Shawn Hutchinson <shutch@ksu.edu>
2025-09-02 09:36:46 -0500

    Update CoordinateCalculator.py

--- CoordinateCalculator.py
+++ CoordinateCalculator.py
@@ -1,6 +1,10 @@
-##-------------------------------------------------------------------------------------
-## Name:  CoordinateCalculator.py
-## Description:  Converts geographic coordinates in DDMMSS format to DD
-## Created:  June 15, 2023
-## Author:  Shawn Hutchinson (shutch@ksu.edu)
-##-------------------------------------------------------------------------------------
+#/ usr/bin/evn python
+# -*- coding: utf-8 -*-
+
+"""
+  Name:  CoordinateCalculator.py
+  Description:  Converts geographic coordinates in DDMMSS format to DD
+  Created:  June 15, 2023
+  Author:  Shawn Hutchinson (shutch@ksu.edu)
+  Python Version:  3.11.11
+"""
